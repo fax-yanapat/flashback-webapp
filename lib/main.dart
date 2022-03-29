212a1a4ac496258e76d105d1651d3c53c2e0afff
Yanapat Limrachadawong <82725241+fax-yanapat@users.noreply.github.com>
2022-03-29 20:07:57 +0700

    draft of webapp complete

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,15 @@ import 'screen/welcome_screen/welcome.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyBJ8pzy4qU6fQny3rcWpn4pFQxlWZfpnBI", 
+      appId: "1:716016674491:web:d4e93a1ca5497fd32f2b9e", 
+      messagingSenderId: "716016674491", 
+      projectId: "flashback-91af7")
+  );
+
   runApp(MyApp());
 }
 
@@ -11,7 +20,6 @@ class MyApp extends StatelessWidget {
   @override
   MyApp({Key? key}) : super(key: key);
   Widget build(BuildContext context) {
-    Firebase.initializeApp();
     return GetMaterialApp(
         title: 'Physics Flashcard',
         theme: ThemeData(
